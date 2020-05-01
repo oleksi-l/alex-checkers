1619f6d09c9cf516bb63762bfc64742c53601d0b
oleksi-l <oleksi.lomakin@gmail.com>
2020-05-02 00:36:41 +0300

    plain checkers can bite

--- src/App.js
+++ src/App.js
@@ -14,22 +14,24 @@ class App extends React.Component {
       checkers: checkers,
       player: "white",
       turns: {},
-      bitedFields: {},
+      toBite: {},
     };
   }
 
   componentDidMount() {
-    this.setState({ turns: this.scanBoard(this.state.player) });
+    let fields = this.scanBoard(this.state.player);
+    this.setState({ turns: fields.turns, toBite: fields.willBeBited });
   }
 
   componentDidUpdate(prevProps, prevState) {
     if (prevState.player !== this.state.player) {
-      this.setState({ turns: this.scanBoard(this.state.player) });
+      let fields = this.scanBoard(this.state.player);
+      this.setState({ turns: fields.turns, toBite: fields.willBeBited });
     }
   }
 
   toggleActive = ({ name, color }) => {
-    let {player,turns} = this.state;
+    let { player, turns } = this.state;
     if (player === color && Object.keys(turns).includes(name)) {
       this.setState({ activeChecker: name });
     }
@@ -37,83 +39,174 @@ class App extends React.Component {
 
   scanBoard = (color) => {
     let turns = {};
-    for (let key in this.state.checkers) {
-      if (this.state.checkers[key].color === color) {
-        let coords = this.getCoords(this.state.checkers[key], "turns");
+    let willBeBited = {};
+    let require = {};
+    let { checkers } = this.state;
+    for (let key in checkers) {
+      if (checkers[key].color === color) {
+        let bited = this.getBitedFields(checkers[key]);
+        let coords = this.getCoords(checkers[key], "turns");
+        if (Object.keys(bited.bited).length > 0) {
+          willBeBited = { ...willBeBited, ...bited.bited };
+          require[key] = bited.turns;
+          continue;
+        }
         if (coords.length > 0) turns[key] = coords;
       }
     }
-    return turns;
+    if (Object.keys(require).length > 0)
+      return { turns: require, willBeBited: willBeBited };
+    return { turns: turns, willBeBited: willBeBited };
   };
 
   getCoords = (checker, action = "turns") => {
     let { x, y, to } = checker;
+    let { freeCells } = this.state;
     y = y - 1;
-    let result = [];
-    let turns = [];
+    let res = [];
     if (action === "turns") {
-      turns =
+      res =
         to === "down"
           ? [`${letters[y - 1]}${x + 1}`, `${letters[y + 1]}${x + 1}`]
           : [`${letters[y - 1]}${x - 1}`, `${letters[y + 1]}${x - 1}`];
     } else {
-      turns = [
+      res = [
         `${letters[y - 1]}${x + 1}`,
         `${letters[y + 1]}${x + 1}`,
         `${letters[y - 1]}${x - 1}`,
         `${letters[y + 1]}${x - 1}`,
       ];
     }
-    for (let i = 0; i < turns.length; i++) {
-      if (
-        turns[i].indexOf("undefined") < 0 &&
-        this.state.freeCells.includes(turns[i])
-      ) {
-        result.push(turns[i]);
-      }
+
+    let result = [];
+
+    for(let i = 0; i < res.length; i++){
+      if(action === "turns" && !freeCells.includes(res[i])) continue;
+      if(res[i].indexOf("undefined") < 0) result.push(res[i]);
     }
+
     return result;
   };
 
-  moveChecker = (field, isActive) => {
-    let { freeCells, activeChecker, checkers, player } = this.state;
-    if (freeCells.includes(field.name) && isActive === "active") {
-      let activeCheckerProps = {
-        ...checkers[activeChecker],
-        name: field.name,
-        x: field.x,
-        y: field.y,
-      };
-      delete checkers[activeChecker];
-      checkers[field.name] = activeCheckerProps;
+  getBitedFields = (checker) => {
+    let curCoords = this.getCoords(checker, "bite");
+    let { checkers, freeCells } = this.state;
+    let result = {};
+    let turns = [];
+    for (let i = 0; i < curCoords.length; i++) {
+      let wrap = {};
+      if (checkers[curCoords[i]]) {
+        if (checkers[curCoords[i]].color === checker.color) continue;
+        else {
+          let enemyY = checkers[curCoords[i]].y;
+          let enemyX = checkers[curCoords[i]].x;
+          let checkY = checker.y < enemyY ? enemyY + 1 : enemyY - 1;
+          let checkX = checker.x < enemyX ? enemyX + 1 : enemyX - 1;
+          let end = `${letters[checkY - 1]}${checkX}`;
+          if (end.indexOf("undefined") > -1 || !freeCells.includes(end))
+            continue;
+          wrap[end] = checkers[curCoords[i]].name;
+          turns.push(end);
+        }
+        result[checker.name] = wrap;
+      }
+    }
+    return { bited: result, turns: turns };
+  };
+
+  replaceChecker = (field) => {
+    let { activeChecker, checkers, freeCells, toBite } = this.state;
+    let activeCheckerProps = {
+      ...checkers[activeChecker],
+      name: field.name,
+      x: field.x,
+      y: field.y,
+    };
+    let wasBited = 0;
+    if (toBite[activeChecker]) {
+      delete checkers[toBite[activeChecker][field.name]];
+      freeCells = freeCells.filter(
+        (item) =>
+          item !== field.name && item !== toBite[activeChecker][field.name]
+      );
+      freeCells.push(toBite[activeChecker][field.name]);
+      wasBited = 1;
+    } else {
       freeCells = freeCells.filter((item) => item !== field.name);
-      freeCells.push(activeChecker);
+    }
+    delete checkers[activeChecker];
+    checkers[field.name] = activeCheckerProps;
+    freeCells.push(activeChecker);
+    return { checkers: checkers, freeCells: freeCells, wasBited: wasBited };
+  };
+
+  biteAgain = (field, player) => {
+    let res = this.scanBoard(player);
+    if (
+      Object.keys(res.turns).length > 0 &&
+      Object.keys(res.willBeBited).length > 0
+    ) {
       this.setState({
-        checkers: checkers,
-        freeCells: freeCells,
-        activeChecker: null,
+        turns: res.turns,
+        toBite: res.willBeBited,
+      });
+    } else {
+      this.setState({
         player: player === "white" ? "black" : "white",
+        activeChecker: null,
       });
     }
   };
 
+  moveChecker = (field, isActive) => {
+    let { freeCells, player } = this.state;
+    if (freeCells.includes(field.name) && isActive === "active") {
+      let { checkers, freeCells, wasBited } = this.replaceChecker(field);
+      if (wasBited === 1) {
+        this.setState(
+          (prev) => ({
+            ...prev.state,
+            activeChecker: field.name,
+            checkers: checkers,
+            freeCells: freeCells,
+          }),
+          () => this.biteAgain(this.state.activeChecker, player)
+        );
+      } else {
+        this.setState({
+          checkers: checkers,
+          freeCells: freeCells,
+          activeChecker: null,
+          player: player === "white" ? "black" : "white",
+        });
+      }
+    }
+  };
+
   render() {
+    const {
+      activeChecker,
+      checkers,
+      freeCells,
+      turns,
+      player,
+      toBite,
+    } = this.state;
     return (
       <AppContext.Provider
         value={{
-          activeChecker: this.state.activeChecker,
-          checkers: this.state.checkers,
-          checkersName: Object.keys(this.state.checkers),
-          freeCells: this.state.freeCells,
-          turns: this.state.activeChecker
-            ? this.state.turns[this.state.activeChecker]
-            : [],
+          activeChecker: activeChecker,
+          checkers: checkers,
+          checkersName: Object.keys(checkers),
+          freeCells: freeCells,
+          turns: activeChecker ? turns[activeChecker] : [],
+          toBite: toBite,
           toggleActive: this.toggleActive,
           moveChecker: this.moveChecker,
         }}
       >
         <div className="App">
-          <h3>{this.state.player}</h3>
+          <h3>{player}</h3>
           <Board />
         </div>
       </AppContext.Provider>
--- src/Cell.jsx
+++ src/Cell.jsx
@@ -2,12 +2,26 @@ import React, { useContext } from "react";
 import Checker from "./Checker";
 import { AppContext } from "./App";
 
+const checkIsActive = (turns, freeCells, name) => {
+  if (!turns || turns.length === 0) return "";
+  let isActive =
+    turns.includes(name) && freeCells.includes(name) ? "active" : "";
+  return isActive;
+};
+
 const Cell = React.memo((props) => {
-  let { checkers, turns, checkersName,moveChecker } = useContext(AppContext);
-  let isActive =  turns.length > 0 && turns.includes(props.name) ? "active" : "";
+  let { checkers, turns, checkersName, moveChecker, freeCells } = useContext(
+    AppContext
+  );
+  let isActive = checkIsActive(turns, freeCells, props.name);
   return (
-    <li className={`row-item ${isActive}`} onClick={() => moveChecker(props,isActive)}>
-      {checkersName.includes(props.name) && <Checker prop={checkers[props.name]} />}
+    <li
+      className={`row-item ${isActive}`}
+      onClick={() => moveChecker(props, isActive)}
+    >
+      {checkersName.includes(props.name) && (
+        <Checker prop={checkers[props.name]} />
+      )}
     </li>
   );
 });
