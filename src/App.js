1c585b82104d900dff328056cc9a71b2f17f8083
oleksi-l <oleksi.lomakin@gmail.com>
2020-06-03 00:23:41 +0300

    change buikd queen route

--- src/App.js
+++ src/App.js
@@ -37,6 +37,14 @@ class App extends React.Component {
     }
   };
 
+  extractCoordsQueen = (arr) => {
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+      result = result.concat(Object.keys(arr[i]));
+    }
+    return result;
+  };
+
   scanBoard = (color) => {
     let turns = {};
     let willBeBited = {};
@@ -46,14 +54,15 @@ class App extends React.Component {
       if (checkers[key].color === color) {
         let bited = this.getBitedFields(checkers[key]);
         let coords = this.getCoords(checkers[key], "turns");
+
         if (checkers[key].isQueen) {
           let tmp = this.getTurnCoordsQueen({
             coords: this.getDiagonal(checkers[key]),
             checker: checkers[key],
           });
-
+          // console.log(tmp);
           if (Object.keys(tmp.toBite).length > 0) {
-            bited.turns = Object.keys(tmp.toBite);
+            bited.turns = this.extractCoordsQueen(tmp.toBite);
             bited.bited[key] = tmp.toBite;
           } else {
             bited.turns = tmp.turns;
@@ -79,10 +88,8 @@ class App extends React.Component {
     y = y - 1;
     let res = [];
     if (action === "turns") {
-      res =
-        to === "down"
-          ? [`${letters[y - 1]}${x + 1}`, `${letters[y + 1]}${x + 1}`]
-          : [`${letters[y - 1]}${x - 1}`, `${letters[y + 1]}${x - 1}`];
+      let add = to === "down" ? 1 : -1;
+      res = [`${letters[y - 1]}${x + add}`, `${letters[y + 1]}${x + add}`];
     } else {
       res = [
         `${letters[y - 1]}${x + 1}`,
@@ -188,6 +195,7 @@ class App extends React.Component {
           checkers[field.name].isQueen = true;
         if (checkers[field.name].to === "up" && field.x === 1)
           checkers[field.name].isQueen = true;
+      } else {
       }
       if (wasBited === 1) {
         this.setState(
@@ -218,11 +226,13 @@ class App extends React.Component {
     for (let i = beginY; i <= endY; i++) {
       if (beginX === 0 && flag === "right") break;
       let xNum = flag === "right" ? beginX-- : beginX++;
+      if (xNum > 8) break;
       let name = `${letters[i]}${xNum}`;
-      // if(name === checker.name) continue;
       checker.x + 1 > xNum ? after.push(name) : before.push(name);
     }
-    return before.concat(after);
+    let tmp = before.concat(after);
+    tmp = tmp.filter((item) => item.indexOf("undefined") < 0);
+    return tmp.sort();
   };
 
   getLeftDiagonal = (checker) => {
@@ -242,7 +252,7 @@ class App extends React.Component {
       endX = 7 - beginY;
     }
     if (x > y) {
-      beginX = x === 7 ? 7 : x - y;
+      beginX = x - y;
       beginY = y - y;
       endY = x === 7 ? y - 1 : 7 - beginX;
       endX = beginY === 0 || x === 7 ? 7 : 7 - beginY;
@@ -275,22 +285,20 @@ class App extends React.Component {
       if (x === 0) beginY = 0;
       else {
         beginY = y < 4 ? y - y : y - (7 - x);
-        beginY = beginY < 0 ? 7 : beginY;
       }
       beginX = y === 7 ? 7 : x + y;
-      beginX = beginX > 7 ? 7 : beginX;
     }
 
     if (x > y) {
-      beginY = y === 0 ? 0 : y - (7 - x);
       if (x === 7) beginY = y;
       else {
-        beginY = y < 0 ? 0 : beginY;
+        beginY = y === 0 ? 0 : y - (7 - x);
       }
       beginX = x + y;
-      beginX = beginX > 7 ? 7 : beginX;
     }
 
+    beginX = beginX > 7 ? 7 : beginX;
+    beginY = beginY < 0 ? 0 : beginY;
     endY = beginX;
     endX = beginY;
 
@@ -317,7 +325,6 @@ class App extends React.Component {
     let turns = [];
     let bited = {};
     let deprecated = [];
-
     for (let i = 0; i < coord.length; i++) {
       if (freeCells.includes(coord[i])) {
         if (counter > 1) break;
@@ -330,12 +337,15 @@ class App extends React.Component {
           if (checkers[coord[i]].color !== player) {
             counter++;
             if (counter > 1) break;
+            if (coord[i + 1] && checkers[coord[i + 1]]) {
+              if (checkers[coord[i + 1]].color === player) break;
+            }
             if (coord[i + 1] && freeCells.includes(coord[i + 1])) {
-              if (+coord[i][1] === 8 || +coord[i][1] === 0) break;
+              if (+coord[i][1] === 8 || +coord[i][1] === 1) break;
               if (coord[i][0] === "a" || coord[i][0] === "h") break;
               bited[coord[i + 1]] = coord[i];
             }
-          }
+          } else break;
         }
       }
     }
@@ -344,86 +354,58 @@ class App extends React.Component {
   };
 
   hasPerspectiveToBite = (params) => {
-    let {
-      coords,
-      freeCells,
-      checkers,
-      deprecated,
-      counter,
-      acc,
-      player,
-    } = params;
-    let tmp = {};
-    tmp.acc = acc;
-    tmp.freeCells = freeCells.filter((item) => !deprecated.includes(item));
-    tmp.checkers = checkers;
-    tmp.toDelete = params.toDelete;
-
-    for (let i = 0; i < coords.length; i++) {
+    let { coords, freeCells, checkers, player, to } = params;
+    let coords1 = Object.keys(coords);
+    let flag = 0;
+    let will = {};
+    let del = {};
+    for (let i = 0; i < coords1.length; i++) {
       let checker = {
-        name: coords[i],
-        x: cells[coords[i]].x,
-        y: cells[coords[i]].y,
+        name: coords1[i],
+        x: cells[coords1[i]].x,
+        y: cells[coords1[i]].y,
         color: player,
       };
 
       let newCoords = this.getDiagonal(checker);
-      for (let key in newCoords) {
-        let bited = this.checkForQueen({
-          checker: checker,
-          coords: newCoords[key],
-          freeCells: freeCells,
-          checkers: checkers,
-          player: player,
-        });
+      let bited = this.checkForQueen({
+        checker: checker,
+        coords: newCoords[to],
+        freeCells: freeCells,
+        checkers: checkers,
+        player: player,
+      });
 
-        if (Object.keys(bited.bited).length === 0) {
-          if (coords.length > 1) tmp.toDelete.push(checker.name);
-          continue;
-        } else {
-          tmp.acc = { ...acc, ...bited.bited };
-          tmp.coords = Object.keys(bited.bited);
-          tmp.deprecated = [...deprecated, ...bited.deprecated];
-        }
+      if (Object.keys(bited.bited).length > 0) {
+        will[coords1[i]] = coords[coords1[i]];
+        flag++;
+      } else {
+        del[coords1[i]] = coords[coords1[i]];
       }
     }
-    if (tmp.coords && tmp.coords.length > 0) {
-      counter++;
-      tmp.counter = counter;
-      if (counter > Object.keys(tmp.acc).length) return tmp.acc;
-      this.hasPerspectiveToBite(tmp);
-    }
-    return { toBite: tmp.acc, toDelete: [...new Set(tmp.toDelete)] };
+    if (Object.keys(will).length > 0) return {will:will,del:{}};
+    else return {will:{},del:del};
   };
 
   checkForQueen = (params) => {
-    let { checker, coords, freeCells, checkers, player } = params;
+    let { checker, coords, freeCells, checkers, player, to } = params;
     let turns = [];
     let bited = {};
-    let deprecated = [];
-    coords = coords.sort();
     const ch = coords.indexOf(checker.name);
-    let before = ch === 0 ? coords.slice(ch) : coords.slice(0, ch);
+    let before = coords.slice(0, ch);
+    before = before.reverse();
     let after = coords.slice(ch + 1);
-    // первичная проверка
     let beforeCoords = this.getCFCheck(before, freeCells, checkers, player);
     let afterCoords = this.getCFCheck(after, freeCells, checkers, player);
     bited = Object.assign({}, beforeCoords.bited, afterCoords.bited);
     turns = [...new Set(turns.concat(beforeCoords.turns, afterCoords.turns))];
-    deprecated = [
-      ...new Set(
-        deprecated.concat(beforeCoords.deprecated, afterCoords.deprecated)
-      ),
-    ];
-    return { turns: turns, bited: bited, deprecated: deprecated };
+    return { turns: turns, bited: bited };
   };
 
   getTurnCoordsQueen = ({ coords, checker }) => {
     let { freeCells, checkers, player } = this.state;
     let turns = [];
     let toBite = {};
-    let deprecated = [];
-
     for (let key in coords) {
       let tmp = this.checkForQueen({
         checker: checker,
@@ -431,33 +413,40 @@ class App extends React.Component {
         freeCells: freeCells,
         checkers: checkers,
         player: player,
+        to: key,
       });
       if (Object.keys(tmp.bited).length > 0) {
-        toBite = { ...toBite, ...tmp.bited };
+        let params = {
+          coords: tmp.bited,
+          freeCells: freeCells,
+          checkers: checkers,
+          player: player,
+          to: key,
+        };
+        let tmp1 = this.hasPerspectiveToBite(params);
+        if(Object.keys(tmp1.del).length > 0){
+          params = {...params, coords:tmp1.del, to:key === "left" ? "right":"left"};
+          let oppositeCoords = this.hasPerspectiveToBite(params);
+          if(Object.keys(oppositeCoords.will).length > 0){
+            toBite = Object.assign(toBite,oppositeCoords.will);
+          }
+          if(Object.keys(oppositeCoords.del).length > 0){
+            toBite = Object.assign(toBite,oppositeCoords.del);
+          }
+        } else {
+          toBite = Object.assign(toBite, tmp1.will);
+        }
+      } else {
+        turns = turns.concat(tmp.turns);
       }
-      turns = turns.concat(tmp.turns);
-      deprecated = deprecated.concat(tmp.deprecated);
     }
     if (Object.keys(toBite).length > 0) {
-      let params = {
-        coords: Object.keys(toBite),
-        freeCells: freeCells,
-        checkers: checkers,
-        deprecated: deprecated,
-        counter: 0,
-        acc: toBite,
-        toDelete: [],
-        player: player,
-      };
-      let result = this.clearInvalidRoutes(this.hasPerspectiveToBite(params));
-      return { turns: [], toBite: result };
-    } else return { turns: turns, toBite: {} };
-  };
-
-  clearInvalidRoutes = ({ toBite, toDelete }) => {
-    for (let checker in toBite) {
-      if (!toDelete.includes(checker)) return { [checker]: toBite[checker] };
-    }
+      let res = [];
+      for (let y in toBite) {
+        res.push({ [y]: toBite[y] });
+      }
+      return { turns: [], toBite: res };
+    } else return { turns: turns, toBite: [] };
   };
 
   render() {
