105af2a8a065b8b8503d77d3598cf3f7406b209a
oleks-l <oleksi.lomakin@gmail.com>
2020-06-15 12:21:24 +0300

    Updates

---
+++ precache-manifest.970ae3e341cfc6e6bddc7f81ff9c97d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37f2c913da95f052d7c736f53d93dda8",
+    "url": "/alex-checkers/index.html"
+  },
+  {
+    "revision": "02ef200275b7eaee3c29",
+    "url": "/alex-checkers/static/css/main.fa78fff3.chunk.css"
+  },
+  {
+    "revision": "650163ec3c341ab3c2ef",
+    "url": "/alex-checkers/static/js/2.d1678d02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/alex-checkers/static/js/2.d1678d02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02ef200275b7eaee3c29",
+    "url": "/alex-checkers/static/js/main.e25bbbd8.chunk.js"
+  },
+  {
+    "revision": "88aaf60831e72506d384",
+    "url": "/alex-checkers/static/js/runtime-main.53e6ee67.js"
+  },
+  {
+    "revision": "4a25f75f367983ed3f1b8c407e326dc7",
+    "url": "/alex-checkers/static/media/queen.4a25f75f.svg"
+  }
+]);
